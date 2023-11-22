5876fabcecf76ad34dc5a2edfb1c68a20b986e2d
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2023-11-22 19:42:21 +0000

    Added request endpoints

--- src/api/data.js
+++ src/api/data.js
@@ -3,3 +3,38 @@ import * as api from './api.js';
 export const login = api.login;
 export const register = api.register;
 export const logout = api.logout;
+
+export async function getAllProperties() {
+    return api.get('/properties')
+}
+
+export async function createProperty(data) {
+    return api.post('/properties', data)
+}
+
+export async function myProperties() {
+    return api.post('/properties/my-properties')
+}
+
+export async function getPropertyById(id) {
+    return api.get(`/properties/${id}`)
+}
+
+export async function editProperty(id) {
+    return api.patch(`/properties/${id}/edit`)
+}
+
+export async function deleteProperty(id) {
+    return api.del(`/properties/${id}/delete`)
+}
+
+export async function canRate(data) {
+    return api.post(`/rate/check-rating`, data)
+}
+
+export async function rateProperty(property_id, data) {
+    return api.post(`/rate/${property_id}`, data)
+}
+
+
+
